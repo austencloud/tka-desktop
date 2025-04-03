9240fa1a2d4731773582693860b9dffeed1b6375
austencloud <68725581+austencloud@users.noreply.github.com>
2025-04-03 01:58:39 -0500

    - Updated image file for snowfall background. - Added new placement for (s, 0.5, 0.5) in from_layer3_blue2_red1 placements. - Added dash property to existing placements in from_layer1. - Refactored thumbnail index access in SequenceViewerActionButtonPanel. - Cleaned up commented-out code in SnowfallBackground class.

--- src/main_window/main_widget/main_background_widget/backgrounds/snowfall/new/snowfall_background.py
+++ src/main_window/main_widget/main_background_widget/backgrounds/snowfall/new/snowfall_background.py
@@ -15,25 +15,23 @@ if TYPE_CHECKING:
 
 class SnowfallBackground(QWidget):
     snowflake_count = 100
-    update_required = pyqtSignal()  # Signal for requesting an update
+    update_required = pyqtSignal()
 
     def __init__(self, main_widget: "MainWidget" = None):
         super().__init__(main_widget)
         self.main_widget = main_widget
 
-        # Dimensions and properties
         self.widget_width = self.main_widget.width()
         self.widget_height = self.main_widget.height()
         self.setFixedSize(self.widget_width, self.widget_height)
         self.setAutoFillBackground(True)
 
-        # Snowflake properties
         self.snowflake_images = [
-            QPixmap(get_image_path(f"snowflakes/snowflake{i}.png")) for i in range(1, 21)
+            QPixmap(get_image_path(f"snowflakes/snowflake{i}.png"))
+            for i in range(1, 21)
         ]
         self.snowflakes = []
 
-        # Worker thread setup
         self.worker_thread = QThread()
         self.worker = SnowflakeWorker(
             self.snowflake_count,
@@ -45,49 +43,15 @@ class SnowfallBackground(QWidget):
         self.worker.update_snowflakes.connect(self._update_snowflakes_from_worker)
         self.worker_thread.started.connect(self.worker.process)
 
-        # Animation timer
-        # self.animation_timer = QTimer()
-        # self.animation_timer.timeout.connect(self.update)
-        # self.animation_timer.timeout.connect(self.animate_background)
-
-        # Additional managers
         self.shooting_star_manager = ShootingStarManager()
         self.santa_manager = SantaManager()
 
-    # def start_animation(self):
-    #     """Start the background animation."""
-    #     self.worker.running = True
-    #     self.worker_thread.start()
-    #     self.animation_timer.start(16)  # Approximately 60 FPS
-
-    # def stop_animation(self):
-    #     """Stop the background animation."""
-    #     self.worker.running = False
-    #     self.worker_thread.quit()
-    #     self.worker_thread.wait()
-    #     self.animation_timer.stop()
-
-    # def closeEvent(self, event):
-    #     """Stop the worker thread and timer when the widget is closed."""
-    #     self.stop_animation()
-    #     super().closeEvent(event)
-
     def paint_background(self, parent_widget: QWidget, painter: QPainter):
-        # Ensure gradient, snowflakes, Santa, and shooting stars are all drawn
+
         gradient = QLinearGradient(0, 0, 0, self.widget_height)
-        gradient.setColorAt(0, QColor(20, 30, 48))  # Dark blue
-        gradient.setColorAt(1, QColor(50, 80, 120))  # Light blue
+        gradient.setColorAt(0, QColor(20, 30, 48))
+        gradient.setColorAt(1, QColor(50, 80, 120))
         painter.fillRect(self.main_widget.rect(), gradient)
-
-        # # Draw Snowflakes
-        # for snowflake in self.snowflakes:
-        #     x, y, image_index = snowflake["x"], snowflake["y"], snowflake["image_index"]
-        #     pixmap = self.snowflake_images[image_index]
-        #     painter.drawPixmap(int(x), int(y), pixmap)
-
-        # # Draw Shooting Stars and Santa
-        # self.shooting_star_manager.draw_shooting_star(painter, parent_widget)
-        # self.santa_manager.draw_santa(painter, parent_widget)
 
     @pyqtSlot(list)
     def _update_snowflakes_from_worker(self, snowflakes):
@@ -112,7 +76,3 @@ class SnowfallBackground(QWidget):
 
         QTimer.singleShot(0, update_bounds)
         super().resizeEvent(event)
-
-    # def _on_animation_tick(self):
-    #     self.animate_background()  # Animate Santa and shooting stars
-    #     self.update()  # Trigger repaint
